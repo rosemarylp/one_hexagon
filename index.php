18a6afc571db200480b6a7083ca05bc7802fd47b
Rosemary <perkinsrosemaryl@gmail.com>
2015-07-26 21:52:41 -0700

    Start static HTML and CSS for index page

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>One Hexagon</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<header>
+		<h1>OneHexagon</h1>
+		<nav>
+			<ul>
+				<li><a href="#">About</a></li>
+				<li><a href="#">Contact</a></li>
+				<li><a href="#">Health &amp; Fitness</a></li>
+				<li><a href="#">Outfits</a></li>
+				<li><a href="#">Home Decor</a></li>
+				<li><a href="#">Wishlist</a></li>
+			</ul>
+			<div class="clear-fix"></div>
+		</nav>
+	</header>
+
+	<main>
+		<section class="categories"></section>
+		<article class="post-text">
+			<h2>Post Title</h2>
+			<h3>July 25 2015</h3>
+			<p class="feed-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam distinctio veniam voluptatum commodi architecto aut est, expedita dolores nobis, suscipit odio explicabo ipsa optio harum tenetur, reiciendis delectus ipsum, consectetur!</p>
+			<img src="images/lorem_small.jpg" alt="" class="img-text-featured">
+			<div class="clear-fix"></div>
+
+			<a href="#" class="link-more">Read More</a>
+		</article>
+		<div class="clear-fix"></div>
+		<article class="post-image">
+			<h2>Post Title</h2>
+			<h3>July 25 2015</h3>
+			<p class="img-feed-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam distinctio veniam voluptatum commodi architecto aut est, expedita dolores nobis, suscipit odio explicabo ipsa optio harum tenetur, reiciendis delectus ipsum, consectetur!</p>
+			<img src="images/lorem.jpg" alt="" class="img-post-featured">
+			<div class="clear-fix"></div>
+
+			<a href="#" class="link-more">Read More</a>
+
+		</article>
+
+	</main>
+
+	<footer>
+		<nav>
+			<ul>
+				<li><a href="#">About</a></li>
+				<li><a href="#">Contact</a></li>
+				<li><a href="#">Health &amp; Fitness</a></li>
+				<li><a href="#">Outfits</a></li>
+				<li><a href="#">Home Decor</a></li>
+				<li><a href="#">Wishlist</a></li>
+			</ul>
+			<div class="clear-fix"></div>
+		</nav>
+	</footer>
+</body>
+</html>
