917fbfec4b03d42f6f51b3623951631383601713
Rosemary <perkinsrosemaryl@gmail.com>
2015-07-27 16:59:53 -0700

    Improve appearance of image posts in feed

--- index.php
+++ index.php
@@ -27,20 +27,26 @@
 			<h2>Post Title</h2>
 			<h3>July 25 2015</h3>
 			<p class="feed-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam distinctio veniam voluptatum commodi architecto aut est, expedita dolores nobis, suscipit odio explicabo ipsa optio harum tenetur, reiciendis delectus ipsum, consectetur!</p>
-			<img src="images/lorem_small.jpg" alt="" class="img-text-featured">
+			<figure class="img-text-featured">
+				<img src="images/lorem_small.jpg" alt="" class="img-text-featured">
+			</figure>
 			<div class="clear-fix"></div>
 
 			<a href="#" class="link-more">Read More</a>
 		</article>
 		<div class="clear-fix"></div>
-		<article class="post-image">
+		<article class="post-img">
 			<h2>Post Title</h2>
 			<h3>July 25 2015</h3>
-			<img src="images/lorem.jpg" alt="" class="img-post-featured">
-			<p class="img-feed-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam distinctio veniam voluptatum commodi architecto aut est, expedita dolores nobis, suscipit odio explicabo ipsa optio harum tenetur, reiciendis delectus ipsum, consectetur!</p>
+			<figure  class="img-post-featured">
+				<img src="images/lorem.jpg" alt="">
+				<figcaption class="img-feed-description">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam distinctio veniam voluptatum commodi architecto aut est, expedita dolores nobis, suscipit odio explicabo ipsa optio harum tenetur, reiciendis delectus ipsum, consectetur!
+				</figcaption>
 			<div class="clear-fix"></div>
 
 			<a href="#" class="link-more">Read More</a>
+			</figure>
 
 		</article>
 
