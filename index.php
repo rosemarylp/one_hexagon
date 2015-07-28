e648d528ffa66de9bc65ab5eb7abb84599e94b0d
Rosemary <perkinsrosemaryl@gmail.com>
2015-07-27 17:50:40 -0700

    Add category images and addt'l font

--- index.php
+++ index.php
@@ -22,9 +22,27 @@
 	</header>
 
 	<main>
-		<section class="categories"></section>
+		<section class="categories-container">
+			<section class="category-health">
+				<a href="#"><img src="images/lorem_small.jpg" alt="" class="categories-unselected"></a>
+				<h2><a href="#">Health &amp; Fitness</a></h2>
+			</section>
+
+			<section class="category-outfits">
+				<a href="#"><img src="images/lorem_small.jpg" alt="" class="categories-unselected"></a>
+				<h2><a href="#">Outfits</a></h2>
+			</section>
+
+			<section class="category-decor">
+				<a href="#"><img src="images/lorem_small.jpg" alt="" class="categories-unselected"></a>
+				<h2><a href="#">Home Decor</a></h2>
+			</section>
+		</section>
+		<div class="clear-fix"></div>
+		<hr/>
+
 		<article class="post-text">
-			<h2>Post Title</h2>
+			<h2><a href="#">Post Title</a></h2>
 			<h3>July 25 2015</h3>
 			<p class="feed-description">
 				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam distinctio veniam voluptatum commodi architecto aut est, expedita dolores nobis, suscipit odio explicabo ipsa optio harum tenetur, reiciendis delectus ipsum, consectetur!  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae, velit. Debitis similique veniam laboriosam nam fugit iusto exercitationem, iure, commodi, qui a tenetur corrupti hic odio pariatur neque cupiditate cum.
@@ -36,9 +54,11 @@
 
 			<a href="#" class="link-more">Read More</a>
 		</article>
+
 		<div class="clear-fix"></div>
+
 		<article class="post-img">
-			<h2>Post Title</h2>
+			<h2><a href="#">Post Title</a></h2>
 			<h3>July 25 2015</h3>
 			<figure  class="img-post-featured">
 				<img src="images/lorem.jpg" alt="">
