db02a6e377f5573f78cce9b0e0e6f8629de1d5f9
Rosemary <perkinsrosemaryl@gmail.com>
2015-07-28 00:53:47 -0700

    Add category page

---
+++ category.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>One Hexagon</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<header>
+		<h1>OneHexagon</h1>
+		<nav>
+			<ul>
+				<li><a href="#">About</a></li>
+				<li><a href="#">Contact</a></li>
+				<li><a href="#">Health &amp; Fitness</a></li>
+				<li><a href="#">Outfits</a></li>
+				<li><a href="#">Home Decor</a></li>
+				<li><a href="#">Wishlist</a></li>
+			</ul>
+			<div class="clear-fix"></div>
+		</nav>
+	</header>
+
+	<main>
+		<h2 class="category-name">Category Name</h2>
+
+		<article class="post-text">
+			<h2><a href="#">Post Title</a></h2>
+			<h3>July 25 2015</h3>
+			<p class="feed-description">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam distinctio veniam voluptatum commodi architecto aut est, expedita dolores nobis, suscipit odio explicabo ipsa optio harum tenetur, reiciendis delectus ipsum, consectetur!  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae, velit. Debitis similique veniam laboriosam nam fugit iusto exercitationem, iure, commodi, qui a tenetur corrupti hic odio pariatur neque cupiditate cum.
+			</p>
+			<figure class="img-text-featured">
+				<img src="images/lorem_small.jpg" alt="" class="img-text-featured">
+			</figure>
+			<div class="clear-fix"></div>
+
+			<a href="#" class="link-more">Read More</a>
+		</article>
+
+		<div class="clear-fix"></div>
+
+		<article class="post-img">
+			<h2><a href="#">Post Title</a></h2>
+			<h3>July 25 2015</h3>
+			<figure  class="img-post-featured">
+				<img src="images/lorem.jpg" alt="">
+				<figcaption class="img-feed-description">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam distinctio veniam voluptatum commodi architecto aut est, expedita dolores nobis, suscipit odio explicabo ipsa optio harum tenetur, reiciendis delectus ipsum, consectetur!
+				</figcaption>
+			<div class="clear-fix"></div>
+
+			<a href="#" class="link-more">Read More</a>
+			</figure>
+		</article>
+
+	</main>
+
+	<footer>
+		<nav>
+			<ul>
+				<li><a href="#">About</a></li>
+				<li><a href="#">Contact</a></li>
+				<li><a href="#">Health &amp; Fitness</a></li>
+				<li><a href="#">Outfits</a></li>
+				<li><a href="#">Home Decor</a></li>
+				<li><a href="#">Wishlist</a></li>
+			</ul>
+			<div class="clear-fix"></div>
+		</nav>
+	</footer>
+</body>
+</html>
