7e570a510e0fcbd70e28c25cc8e19e691f9d2ed5
Rosemary <perkinsrosemaryl@gmail.com>
2015-07-28 23:50:13 -0700

    Add post view

---
+++ post.php
@@ -0,0 +1,30 @@
+<?php 
+	$page_heading = "Post Title";
+	require_once 'inc/header.inc.php';
+
+ ?>
+		<article class="post-text">
+			<h3>July 25 2015</h3>
+			<div class="col-seventy">
+				<p class="post-paragraph">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam distinctio veniam voluptatum commodi architecto aut est, expedita dolores nobis, suscipit odio explicabo ipsa optio harum tenetur, reiciendis delectus ipsum, consectetur!  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae, velit. Debitis similique veniam laboriosam nam fugit iusto exercitationem, iure, commodi, qui a tenetur corrupti hic odio pariatur neque cupiditate cum.
+				</p>
+				<p class="post-paragraph">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam distinctio veniam voluptatum commodi architecto aut est, expedita dolores nobis, suscipit odio explicabo ipsa optio harum tenetur, reiciendis delectus ipsum, consectetur!  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae, velit. Debitis similique veniam laboriosam nam fugit iusto exercitationem, iure, commodi, qui a tenetur corrupti hic odio pariatur neque cupiditate cum.
+				</p>
+				<p class="post-paragraph">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam distinctio veniam voluptatum commodi architecto aut est, expedita dolores nobis, suscipit odio explicabo ipsa optio harum tenetur, reiciendis delectus ipsum, consectetur!  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae, velit. Debitis similique veniam laboriosam nam fugit iusto exercitationem, iure, commodi, qui a tenetur corrupti hic odio pariatur neque cupiditate cum.
+				</p>
+				<p class="post-paragraph">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam distinctio veniam voluptatum commodi architecto aut est, expedita dolores nobis, suscipit odio explicabo ipsa optio harum tenetur, reiciendis delectus ipsum, consectetur!  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae, velit. Debitis similique veniam laboriosam nam fugit iusto exercitationem, iure, commodi, qui a tenetur corrupti hic odio pariatur neque cupiditate cum.
+				</p>
+			</div>
+			<figure>
+				<img src="images/lorem_small.jpg" alt="" class="img-text-featured">
+			</figure>
+			<div class="clear-fix"></div>
+
+			<a href="#" class="link-more">More Category Name</a>
+		</article>
+
+<?php require_once 'inc/footer.inc.php'; ?>
