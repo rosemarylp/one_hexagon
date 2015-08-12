ed65842e24ffbd2e00be6229aab65e869ed5ac9c
Rosemary <perkinsrosemaryl@gmail.com>
2015-08-12 13:42:17 -0700

    Add header includes for admin functions

---
+++ inc/nav_admin.inc.php
@@ -0,0 +1,18 @@
+<nav>
+	<ul>
+		<li><a href="about.php">About</a></li>
+		<li><a href="contact.php">Contact</a></li>
+		<li><a href="category.php">Health &amp; Fitness</a></li>
+		<li><a href="category.php">Outfits</a></li>
+		<li><a href="category.php">Home Decor</a></li>
+		<li><a href="wishlist.php">Wishlist</a></li>
+		<li><a href="#">Admin Tools</a>
+			<ul>
+				<li><a href="manage_posts.php">Manage Posts</a></li>
+				<li><a href="manage_categories">Manage Categories</a></li>
+				<li><a href="manage_images">Manage Images</a></li>
+			</ul></li>
+		<li><a href="add_post.php">+New Post</a></li>
+	</ul>
+	<div class="clear-fix"></div>
+</nav>
