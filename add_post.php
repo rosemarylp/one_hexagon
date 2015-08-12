97670ef6f50da640f041e947eff76ddbe7816c90
Rosemary <perkinsrosemaryl@gmail.com>
2015-08-12 14:19:36 -0700

    Add preliminary styling for admin nav and add_post

--- add_post.php
+++ add_post.php
@@ -2,6 +2,28 @@
 $page_heading = "Add Post";
 require_once 'inc/functions.inc.php';
 require_once 'inc/header_admin.inc.php';
-
-
  ?>
+
+<form action="add_post.php">
+	<p>
+		<label for="category">Category: </label>
+		<select name="category">
+			<option value="health">Health &amp; Fitness</option>
+			<option value="outfits">Outfits</option>
+			<option value="decor">Home Decor</option>
+		</select>
+	</p>
+	<p>
+		<label for="title">Post Title: </label>
+		<input type="text" name="title">
+	</p>
+
+	<p>
+		<label for="post-content">Content: </label>
+		<textarea name="post-content" id="" cols="30" rows="10"></textarea>
+	</p>
+
+	<input type="submit" name="submit" value="Submit">
+</form>
+
+<?php require_once 'inc/footer.inc.php'; ?>
--- inc/nav_admin.inc.php
+++ inc/nav_admin.inc.php
@@ -1,4 +1,4 @@
-<nav>
+<nav class="admin_nav">
 	<ul>
 		<li><a href="about.php">About</a></li>
 		<li><a href="contact.php">Contact</a></li>
@@ -6,7 +6,7 @@
 		<li><a href="category.php">Outfits</a></li>
 		<li><a href="category.php">Home Decor</a></li>
 		<li><a href="wishlist.php">Wishlist</a></li>
-		<li><a href="#">Admin Tools</a>
+		<li class="nested"><a href="#">Admin Tools</a>
 			<ul>
 				<li><a href="manage_posts.php">Manage Posts</a></li>
 				<li><a href="manage_categories">Manage Categories</a></li>
