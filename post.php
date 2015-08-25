7a910b5e5a3b7feef80ef1bfabb689adfa6601c8
Rosemary <perkinsrosemaryl@gmail.com>
2015-08-24 20:24:14 -0700

    post.php outputs post content from db based on post id

--- post.php
+++ post.php
@@ -1,23 +1,24 @@
 <?php 
-	$page_heading = "Post Title";
-	require_once 'inc/header.inc.php';
+	require_once 'inc/functions.inc.php';
+
+	if (isset($_GET["post_id"])) {
+		$post_id = $_GET["post_id"];
+		$sql = "SELECT * FROM posts WHERE id=?";
+		$parameters = [$post_id];
+		$post = get_records($connection, $sql, $parameters);
+		$page_heading = $post[0]["title"];
+		require_once 'inc/header.inc.php';
+
+
+		foreach ($post as $post_content) {
+			# code...
+		
 
  ?>
 		<article class="post-text">
-			<h3>July 25 2015</h3>
+			<h3><?php echo $post_content["date"]; ?></h3>
 			<div class="col-seventy">
-				<p class="post-paragraph">
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam distinctio veniam voluptatum commodi architecto aut est, expedita dolores nobis, suscipit odio explicabo ipsa optio harum tenetur, reiciendis delectus ipsum, consectetur!  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae, velit. Debitis similique veniam laboriosam nam fugit iusto exercitationem, iure, commodi, qui a tenetur corrupti hic odio pariatur neque cupiditate cum.
-				</p>
-				<p class="post-paragraph">
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam distinctio veniam voluptatum commodi architecto aut est, expedita dolores nobis, suscipit odio explicabo ipsa optio harum tenetur, reiciendis delectus ipsum, consectetur!  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae, velit. Debitis similique veniam laboriosam nam fugit iusto exercitationem, iure, commodi, qui a tenetur corrupti hic odio pariatur neque cupiditate cum.
-				</p>
-				<p class="post-paragraph">
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam distinctio veniam voluptatum commodi architecto aut est, expedita dolores nobis, suscipit odio explicabo ipsa optio harum tenetur, reiciendis delectus ipsum, consectetur!  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae, velit. Debitis similique veniam laboriosam nam fugit iusto exercitationem, iure, commodi, qui a tenetur corrupti hic odio pariatur neque cupiditate cum.
-				</p>
-				<p class="post-paragraph">
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam distinctio veniam voluptatum commodi architecto aut est, expedita dolores nobis, suscipit odio explicabo ipsa optio harum tenetur, reiciendis delectus ipsum, consectetur!  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae, velit. Debitis similique veniam laboriosam nam fugit iusto exercitationem, iure, commodi, qui a tenetur corrupti hic odio pariatur neque cupiditate cum.
-				</p>
+				<?php echo $post_content["content"]; ?>
 			</div>
 			<figure>
 				<img src="images/lorem_small.jpg" alt="" class="img-text-featured">
@@ -26,5 +27,8 @@
 
 			<a href="#" class="link-more">More Category Name</a>
 		</article>
+<?php }} else {
+		// redirect_to("index.php");
+	} ?>
 
 <?php require_once 'inc/footer.inc.php'; ?>
